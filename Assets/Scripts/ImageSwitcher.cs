bcf054eb5719f9989b27afa23f433cda24e6b69b
DocHaskins <48019945+DocHaskins@users.noreply.github.com>
2019-06-20 18:08:25 -0400

    Hazmat demo with Bugfix
    Changes:
    -Continued development of hazmat scene, rounding out stage 4 interactions, still not completely finished but working.
    -Fixed bug where game timescale would get set to "0" and pause all of the actions but the game would continue to drive on thanks to playmaker ignoring the pause button.
    
    Comments:
    -Fix for the timescale not being affected by the pause menu button can be found under the SteamVR_Settings.asset
    -Code and demo runs without issue now.

---
+++ Assets/Scripts/ImageSwitcher.cs
@@ -0,0 +1,43 @@
+﻿//Image switcher randomly enables one of a set of objects.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSwitcher : MonoBehaviour
+{
+
+    public GameObject[] objects;
+
+    public float interval = .5f;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > interval)
+        {
+            timer = 0;
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetActive(false);
+            }
+
+            int randomindex =  Random.Range(0, objects.Length);
+
+
+            objects[randomindex].SetActive(true);
+
+        }
+        
+    }
+}
