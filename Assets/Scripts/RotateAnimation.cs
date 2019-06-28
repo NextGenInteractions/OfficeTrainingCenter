748ad8195ae5b0dc503ed91432e6bd90d210d2d3
DocHaskins <48019945+DocHaskins@users.noreply.github.com>
2019-06-28 12:39:18 -0400

    Stable - Updated Working demo with 3 scenes and Demo content
    Changes:
    -Expanded modular code from 1 scene across the comfort, training, and applied scene to better fit with the Chicago demo requirements.
    -Added 3 phases of firefighter character from Comfort to Applied.
    -Added new audio for character
    -Added new button press for scene transitions.
    
    Comments:
    -Need better transition content, currently just loads level.
    -Need to add latest scripts to allow auto detection of tracked objects and leap motion hands.

---
+++ Assets/Scripts/RotateAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAnimation : MonoBehaviour
+{
+
+    public float speed = 1;
+    public Vector3 axis = new Vector3(0, 1, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(axis, Time.deltaTime * speed);
+    }
+}
